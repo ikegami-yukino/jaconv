59a5ba222553c40d349491e4ad62aa61c3f7726f
YukinoIkegami <yukino0131@me.com>
2014-09-24 01:18:58 +0900

    :lipstick: Code formatting

--- setup.py
+++ setup.py
@@ -3,17 +3,19 @@ from distutils.core import setup
 
 
 setup(
-    name = 'jctconv',
-    py_modules = ['jctconv'], 
-    version = '0.0.7.2',
-    license = 'MIT License',        
-    platforms = ['POSIX', 'Windows', 'Unix', 'MacOS'],
-    description = 'Pure-Python Japanese character interconverter for Hiragana, Katakana, Hankaku and Zenkaku',
-    author = 'Yukino Ikegami',
-    author_email = 'yukino0131@me.com',
-    url = 'https://github.com/ikegami-yukino/jctconv',
-    keywords = ['japanese converter', 'half-width kana', 'Hiragana', 'Katakana', 'Hankaku', 'Zenkaku'],
-    classifiers = [
+    name='jctconv',
+    py_modules=['jctconv'],
+    version='0.0.7.3',
+    license='MIT License',
+    platforms=['POSIX', 'Windows', 'Unix', 'MacOS'],
+    description='Pure-Python Japanese character interconverter for '
+                'Hiragana, Katakana, Hankaku and Zenkaku',
+    author='Yukino Ikegami',
+    author_email='yukino0131@me.com',
+    url='https://github.com/ikegami-yukino/jctconv',
+    keywords=['japanese converter', 'half-width kana', 'Hiragana', 'Katakana',
+              'Hankaku', 'Zenkaku'],
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
@@ -21,13 +23,14 @@ setup(
         'Natural Language :: Japanese',
         'Operating System :: MacOS',
         'Operating System :: Microsoft',
-        'Operating System :: POSIX', 
+        'Operating System :: POSIX',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Text Processing'
         ],
-    long_description = open('README.rst').read() + '\n\n' + open('CHANGES.rst').read()
     data_files=[('', ['README.rst', 'CHANGES.rst'])],
+    long_description='%s\n\n%s' % (open('README.rst').read(),
+                                   open('CHANGES.rst').read())
 )
