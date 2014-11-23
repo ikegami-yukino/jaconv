d4a18461a9c28c580ed8619a4db2c154057dde6a
Yukino Ikegami <yukino0131@me.com>
2014-11-24 01:27:49 +0900

    :lipstick: code formatting

--- jctconv/__init__.py
+++ jctconv/__init__.py
@@ -1,4 +1,24 @@
 from . import jctconv
+u"""jctconv
+
+This module provides Japanese and ASCII character interconverting between
+Hiragana and full-/half-width Katakana/ASCII characters.
+
+Author:
+    Yukino Ikegami
+
+Lisence:
+    MIT License
+
+Usage:
+    import jctconv
+    jctconv.hira2kata(text, [ignore]) # ひらがなを全角カタカナに変換
+    jctconv.hira2hkata(text, [ignore]) # ひらがなを半角カタカナに変換
+    jctconv.kata2hira(text, [ignore]) # 全角カタカナをひらがなに変換
+    jctconv.h2z(text, [ignore, kana, ascii, digit]) # 半角文字を全角文字に変換
+    jctconv.z2h(text, [ignore, kana, ascii, digit]) # 全角文字を半角文字に変換
+    jctconv.normalize(text, [nomalizemode]) # 半角カナを全角カナへ、全角英数字を半角英数字に変換
+"""
 
 VERSION = (0, 1)
 __version__ = '0.1'
--- jctconv/jctconv.py
+++ jctconv/jctconv.py
@@ -1,127 +1,63 @@
 # -*- coding: utf-8 -*-
 import unicodedata
 
-u"""
-jctconv
-
-This module provides Japanese and ASCII character interconverting between
-Hiragana and full-/half-width Katakana/ASCII characters.
-
-Author:
-    Yukino Ikegami
-
-Lisence:
-    MIT License
-
-Usage:
-    import jctconv
-    jctconv.hira2kata(text, [ignore]) # ひらがなを全角カタカナに変換
-    jctconv.hira2hkata(text, [ignore]) # ひらがなを半角カタカナに変換
-    jctconv.kata2hira(text, [ignore]) # 全角カタカナをひらがなに変換
-    jctconv.h2z(text, [ignore, kana, ascii, digit]) # 半角文字を全角文字に変換
-    jctconv.z2h(text, [ignore, kana, ascii, digit]) # 全角文字を半角文字に変換
-    jctconv.normalize(text, [nomalizemode]) # 半角カナを全角カナへ、全角英数字を半角英数字に変換
-
-    nomalizemodeはNFC, NFKC, NKD, NFKDから指定可能
-    ignoreで変換除外文字を指定可能
-"""
-
-__CHAR_TABLE = {
-    'HIRAGANA': list(u'ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすず'
-                     u'せぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴ'
-                     u'ふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろわ'
-                     u'をんーゎゐゑゕゖゔ'),
-    'HALF_ASCII': list(u'!"#$%&\'()*+,-./:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-                       u'[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~ '),
-    'HALF_DIGIT': list(u'0123456789'),
-    'HALF_KANA_SEION': list(u'ｧｱｨｲｩｳｪｴｫｵｶｷｸｹｺｻｼｽｾｿﾀﾁｯﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓｬﾔｭﾕｮﾖ'
-                            u'ﾗﾘﾙﾚﾛﾜｦﾝｰヮヰヱヵヶ'),
-    'HALF_KANA': [u'ｧ', u'ｱ', u'ｨ', u'ｲ', u'ｩ', u'ｳ', u'ｪ', u'ｴ', u'ｫ', u'ｵ',
-                  u'ｶ', u'ｶﾞ', u'ｷ', u'ｷﾞ', u'ｸ', u'ｸﾞ', u'ｹ', u'ｹﾞ', u'ｺ',
-                  u'ｺﾞ', u'ｻ', u'ｻﾞ', u'ｼ', u'ｼﾞ', u'ｽ', u'ｽﾞ', u'ｾ', u'ｾﾞ',
-                  u'ｿ', u'ｿﾞ', u'ﾀ', u'ﾀﾞ', u'ﾁ', u'ﾁﾞ', u'ｯ', u'ﾂ', u'ﾂﾞ',
-                  u'ﾃ', u'ﾃﾞ', u'ﾄ', u'ﾄﾞ', u'ﾅ', u'ﾆ', u'ﾇ', u'ﾈ', u'ﾉ', u'ﾊ',
-                  u'ﾊﾞ', u'ﾊﾟ', u'ﾋ', u'ﾋﾞ', u'ﾋﾟ', u'ﾌ', u'ﾌﾞ', u'ﾌﾟ', u'ﾍ',
-                  u'ﾍﾞ', u'ﾍﾟ', u'ﾎ', u'ﾎﾞ', u'ﾎﾟ', u'ﾏ', u'ﾐ', u'ﾑ', u'ﾒ',
-                  u'ﾓ', u'ｬ', u'ﾔ', u'ｭ', u'ﾕ', u'ｮ', u'ﾖ', u'ﾗ', u'ﾘ', u'ﾙ',
-                  u'ﾚ', u'ﾛ', u'ﾜ', u'ｦ', u'ﾝ', u'ｰ',
-                  u'ヮ', u'ヰ', u'ヱ', u'ヵ', u'ヶ', u'ｳﾞ'],
-    'FULL_ASCII': list(u'！＂＃＄％＆＇（）＊＋，－．／：；＜＝＞？＠'
-                       u'ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ'
-                       u'［＼］＾＿｀ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔ'
-                       u'ｕｖｗｘｙｚ｛｜｝～　'),
-    'FULL_DIGIT': list(u'０１２３４５６７８９'),
-    'FULL_KANA': list(u'ァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソ'
-                      u'ゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペ'
-                      u'ホボポマミムメモャヤュユョヨラリルレロワヲンーヮヰヱヵヶヴ'),
-    'FULL_KANA_SEION': list(u'ァアィイゥウェエォオカキクケコサシスセソタチッツテト'
-                            u'ナニヌネノハヒフヘホマミムメモャヤュユョヨラリルレロ'
-                            u'ワヲンーヮヰヱヵヶ')
-}
+HIRAGANA = list(u'ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすず'
+                u'せぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴ'
+                u'ふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろわ'
+                u'をんーゎゐゑゕゖゔ')
+HALF_ASCII = list(u'!"#$%&\'()*+,-./:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+                  u'[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~ ')
+HALF_DIGIT = list(u'0123456789'),
+HALF_KANA_SEION = list(u'ｧｱｨｲｩｳｪｴｫｵｶｷｸｹｺｻｼｽｾｿﾀﾁｯﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓｬﾔｭﾕｮﾖ'
+                       u'ﾗﾘﾙﾚﾛﾜｦﾝｰヮヰヱヵヶ')
+HALF_KANA = [u'ｧ', u'ｱ', u'ｨ', u'ｲ', u'ｩ', u'ｳ', u'ｪ', u'ｴ', u'ｫ', u'ｵ',
+             u'ｶ', u'ｶﾞ', u'ｷ', u'ｷﾞ', u'ｸ', u'ｸﾞ', u'ｹ', u'ｹﾞ', u'ｺ',
+             u'ｺﾞ', u'ｻ', u'ｻﾞ', u'ｼ', u'ｼﾞ', u'ｽ', u'ｽﾞ', u'ｾ', u'ｾﾞ',
+             u'ｿ', u'ｿﾞ', u'ﾀ', u'ﾀﾞ', u'ﾁ', u'ﾁﾞ', u'ｯ', u'ﾂ', u'ﾂﾞ',
+             u'ﾃ', u'ﾃﾞ', u'ﾄ', u'ﾄﾞ', u'ﾅ', u'ﾆ', u'ﾇ', u'ﾈ', u'ﾉ', u'ﾊ',
+             u'ﾊﾞ', u'ﾊﾟ', u'ﾋ', u'ﾋﾞ', u'ﾋﾟ', u'ﾌ', u'ﾌﾞ', u'ﾌﾟ', u'ﾍ',
+             u'ﾍﾞ', u'ﾍﾟ', u'ﾎ', u'ﾎﾞ', u'ﾎﾟ', u'ﾏ', u'ﾐ', u'ﾑ', u'ﾒ',
+             u'ﾓ', u'ｬ', u'ﾔ', u'ｭ', u'ﾕ', u'ｮ', u'ﾖ', u'ﾗ', u'ﾘ', u'ﾙ',
+             u'ﾚ', u'ﾛ', u'ﾜ', u'ｦ', u'ﾝ', u'ｰ',
+             u'ヮ', u'ヰ', u'ヱ', u'ヵ', u'ヶ', u'ｳﾞ']
+FULL_ASCII = list(u'！＂＃＄％＆＇（）＊＋，－．／：；＜＝＞？＠'
+                  u'ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ'
+                  u'［＼］＾＿｀ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔ'
+                  u'ｕｖｗｘｙｚ｛｜｝～　')
+FULL_DIGIT = list(u'０１２３４５６７８９')
+FULL_KANA = list(u'ァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソ'
+                 u'ゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペ'
+                 u'ホボポマミムメモャヤュユョヨラリルレロワヲンーヮヰヱヵヶヴ'),
+FULL_KANA_SEION = list(u'ァアィイゥウェエォオカキクケコサシスセソタチッツテト'
+                       u'ナニヌネノハヒフヘホマミムメモャヤュユョヨラリルレロ'
+                       u'ワヲンーヮヰヱヵヶ')
 
 
-def __to_ord_dict(list_from, list_to):
-    list_from = [ord(char) for char in list_from]
-    return dict(zip(list_from, list_to))
+def _to_ord_dict(_from, _to):
+    _from = map(ord, _from)
+    return dict(zip(map(ord, _from), _to))
 
-__CONV_TABLE = {
-    'H2K_TABLE': __to_ord_dict(__CHAR_TABLE['HIRAGANA'],
-                               __CHAR_TABLE['FULL_KANA']),
-    'H2HK_TABLE': __to_ord_dict(__CHAR_TABLE['HIRAGANA'],
-                                __CHAR_TABLE['HALF_KANA']),
-    'K2H_TABLE': __to_ord_dict(__CHAR_TABLE['FULL_KANA'],
-                               __CHAR_TABLE['HIRAGANA']),
+H2K_TABLE = _to_ord_dict(HIRAGANA, FULL_KANA)
+H2HK_TABLE = _to_ord_dict(HIRAGANA, HALF_KANA)
+K2H_TABLE = _to_ord_dict(FULL_KANA, HIRAGANA)
 
-    'H2Z_A': __to_ord_dict(__CHAR_TABLE['HALF_ASCII'],
-                           __CHAR_TABLE['FULL_ASCII']),
-    'H2Z_AD': __to_ord_dict(__CHAR_TABLE['HALF_ASCII'] +
-                            __CHAR_TABLE['HALF_DIGIT'],
-                            __CHAR_TABLE['FULL_ASCII'] +
-                            __CHAR_TABLE['FULL_DIGIT']),
-    'H2Z_AK': __to_ord_dict(__CHAR_TABLE['HALF_ASCII'] +
-                            __CHAR_TABLE['HALF_KANA_SEION'],
-                            __CHAR_TABLE['FULL_ASCII'] +
-                            __CHAR_TABLE['FULL_KANA_SEION']),
-    'H2Z_D': __to_ord_dict(__CHAR_TABLE['HALF_DIGIT'],
-                           __CHAR_TABLE['FULL_DIGIT']),
-    'H2Z_K': __to_ord_dict(__CHAR_TABLE['HALF_KANA_SEION'],
-                           __CHAR_TABLE['FULL_KANA_SEION']),
-    'H2Z_DK': __to_ord_dict(__CHAR_TABLE['HALF_DIGIT'] +
-                            __CHAR_TABLE['HALF_KANA_SEION'],
-                            __CHAR_TABLE['FULL_DIGIT'] +
-                            __CHAR_TABLE['FULL_KANA_SEION']),
-    'H2Z_ALL': __to_ord_dict(__CHAR_TABLE['HALF_ASCII'] +
-                             __CHAR_TABLE['HALF_DIGIT'] +
-                             __CHAR_TABLE['HALF_KANA_SEION'],
-                             __CHAR_TABLE['FULL_ASCII'] +
-                             __CHAR_TABLE['FULL_DIGIT'] +
-                             __CHAR_TABLE['FULL_KANA_SEION']),
+H2Z_A = _to_ord_dict(HALF_ASCII, FULL_ASCII)
+H2Z_AD = _to_ord_dict(HALF_ASCII+HALF_DIGIT, FULL_ASCII+FULL_DIGIT)
+H2Z_AK = _to_ord_dict(HALF_ASCII+HALF_KANA_SEION, FULL_ASCII+FULL_KANA_SEION)
+H2Z_D = _to_ord_dict(HALF_DIGIT, FULL_DIGIT)
+H2Z_K = _to_ord_dict(HALF_KANA_SEION, FULL_KANA_SEION)
+H2Z_DK = _to_ord_dict(HALF_DIGIT+HALF_KANA_SEION, FULL_DIGIT+FULL_KANA_SEION)
+H2Z_ALL = _to_ord_dict(HALF_ASCII+HALF_DIGIT+HALF_KANA_SEION,
+                       FULL_ASCII+FULL_DIGIT+FULL_KANA_SEION)
 
-    'Z2H_A': __to_ord_dict(__CHAR_TABLE['FULL_ASCII'],
-                           __CHAR_TABLE['HALF_ASCII']),
-    'Z2H_AD': __to_ord_dict(__CHAR_TABLE['FULL_ASCII'] +
-                            __CHAR_TABLE['FULL_DIGIT'],
-                            __CHAR_TABLE['HALF_ASCII'] +
-                            __CHAR_TABLE['HALF_DIGIT']),
-    'Z2H_AK': __to_ord_dict(__CHAR_TABLE['FULL_ASCII'] +
-                            __CHAR_TABLE['FULL_KANA'],
-                            __CHAR_TABLE['HALF_ASCII'] +
-                            __CHAR_TABLE['HALF_KANA']),
-    'Z2H_D': __to_ord_dict(__CHAR_TABLE['FULL_DIGIT'],
-                           __CHAR_TABLE['HALF_DIGIT']),
-    'Z2H_K': __to_ord_dict(__CHAR_TABLE['FULL_KANA'],
-                           __CHAR_TABLE['HALF_KANA']),
-    'Z2H_DK': __to_ord_dict(__CHAR_TABLE['FULL_DIGIT'] +
-                            __CHAR_TABLE['FULL_KANA'],
-                            __CHAR_TABLE['HALF_DIGIT'] +
-                            __CHAR_TABLE['HALF_KANA']),
-    'Z2H_ALL': __to_ord_dict(__CHAR_TABLE['FULL_ASCII'] +
-                             __CHAR_TABLE['FULL_DIGIT'] +
-                             __CHAR_TABLE['FULL_KANA'],
-                             __CHAR_TABLE['HALF_ASCII'] +
-                             __CHAR_TABLE['HALF_DIGIT'] +
-                             __CHAR_TABLE['HALF_KANA'])
-}
+Z2H_A = _to_ord_dict(FULL_ASCII, HALF_ASCII)
+Z2H_AD = _to_ord_dict(FULL_ASCII+FULL_DIGIT, HALF_ASCII+HALF_DIGIT)
+Z2H_AK = _to_ord_dict(FULL_ASCII+FULL_KANA, HALF_ASCII+HALF_KANA)
+Z2H_D = _to_ord_dict(FULL_DIGIT, HALF_DIGIT)
+Z2H_K = _to_ord_dict(FULL_KANA, HALF_KANA)
+Z2H_DK = _to_ord_dict(FULL_DIGIT+FULL_KANA, HALF_DIGIT+HALF_KANA)
+Z2H_ALL = _to_ord_dict(FULL_ASCII+FULL_DIGIT+FULL_KANA,
+                       HALF_ASCII+HALF_DIGIT+HALF_KANA)
 
 
 def hira2kata(text, ignore=''):
@@ -133,7 +69,7 @@ def hira2kata(text, ignore=''):
     Return:
         <unicode> converted_text
     """
-    h2k_hash = _exclude_ignorechar(ignore, __CONV_TABLE['H2K_TABLE'])
+    h2k_hash = _exclude_ignorechar(ignore, H2K_TABLE)
     return _convert(text, h2k_hash)
 
 
@@ -146,7 +82,7 @@ def hira2hkata(text, ignore=''):
     Return:
         <unicode> converted_text
     """
-    h2hk_hash = _exclude_ignorechar(ignore, __CONV_TABLE['H2HK_TABLE'])
+    h2hk_hash = _exclude_ignorechar(ignore, H2HK_TABLE)
     return _convert(text, h2hk_hash)
 
 
@@ -159,7 +95,7 @@ def kata2hira(text, ignore=''):
     Return:
         <unicode> converted_text
     """
-    k2h_hash = _exclude_ignorechar(ignore, __CONV_TABLE['K2H_TABLE'])
+    k2h_hash = _exclude_ignorechar(ignore, K2H_TABLE)
     return _convert(text, k2h_hash)
 
 
@@ -176,33 +112,39 @@ def h2z(text, ignore='', kana=True, ascii=False, digit=False):
         """
         半角濁点カナを全角に変換
         """
-        return text.replace(u"ｶﾞ", u"ガ").replace(u"ｷﾞ", u"ギ").replace(u"ｸﾞ", u"グ").replace(u"ｹﾞ", u"ゲ").replace(u"ｺﾞ", u"ゴ").replace(u"ｻﾞ", u"ザ").replace(u"ｼﾞ", u"ジ").replace(u"ｽﾞ", u"ズ").replace(u"ｾﾞ", u"ゼ").replace(u"ｿﾞ", u"ゾ").replace(u"ﾀﾞ", u"ダ").replace(u"ﾁﾞ", u"ヂ").replace(u"ﾂﾞ", u"ヅ").replace(u"ﾃﾞ", u"デ").replace(u"ﾄﾞ", u"ド").replace(u"ﾊﾞ", u"バ").replace(u"ﾋﾞ", u"ビ").replace(u"ﾌﾞ", u"ブ").replace(u"ﾍﾞ", u"ベ").replace(u"ﾎﾞ", u"ボ").replace(u"ﾊﾟ", u"パ").replace(u"ﾋﾟ", u"ピ").replace(u"ﾌﾟ", u"プ").replace(u"ﾍﾟ", u"ペ").replace(u"ﾎﾟ", u"ポ").replace(u"ｳﾞ", u"ヴ")
+        text = text.replace(u"ｶﾞ", u"ガ").replace(u"ｷﾞ", u"ギ")
+        text = text.replace(u"ｸﾞ", u"グ").replace(u"ｹﾞ", u"ゲ")
+        text = text.replace(u"ｺﾞ", u"ゴ").replace(u"ｻﾞ", u"ザ")
+        text = text.replace(u"ｼﾞ", u"ジ").replace(u"ｽﾞ", u"ズ")
+        text = text.replace(u"ｾﾞ", u"ゼ").replace(u"ｿﾞ", u"ゾ")
+        text = text.replace(u"ﾀﾞ", u"ダ").replace(u"ﾁﾞ", u"ヂ")
+        text = text.replace(u"ﾂﾞ", u"ヅ").replace(u"ﾃﾞ", u"デ")
+        text = text.replace(u"ﾄﾞ", u"ド").replace(u"ﾊﾞ", u"バ")
+        text = text.replace(u"ﾋﾞ", u"ビ").replace(u"ﾌﾞ", u"ブ")
+        text = text.replace(u"ﾍﾞ", u"ベ").replace(u"ﾎﾞ", u"ボ")
+        text = text.replace(u"ﾊﾟ", u"パ").replace(u"ﾋﾟ", u"ピ")
+        text = text.replace(u"ﾌﾟ", u"プ").replace(u"ﾍﾟ", u"ペ")
+        return text.replace(u"ﾎﾟ", u"ポ").replace(u"ｳﾞ", u"ヴ")
 
-    if kana and ascii and digit:
-        h2z_hash = __CONV_TABLE['H2Z_ALL']
-        text = _conv_dakuten(text)
-    else:
-        if ascii:
-            if digit:
-                if kana:
-                    h2z_hash = __CONV_TABLE['H2Z_ALL']
-                    text = _conv_dakuten(text)
-                else:
-                    h2z_hash = __CONV_TABLE['H2Z_AD']
-            elif kana:
-                h2z_hash = __CONV_TABLE['H2Z_AK']
-                text = _conv_dakuten(text)
-            else:
-                h2z_hash = __CONV_TABLE['H2Z_A']
-        elif digit:
+    if ascii:
+        if digit:
             if kana:
-                h2z_hash = __CONV_TABLE['H2Z_DK']
-                text = _conv_dakuten(text)
+                h2z_hash = H2Z_ALL
             else:
-                h2z_hash = __CONV_TABLE['H2Z_D']
+                h2z_hash = H2Z_AD
+        elif kana:
+            h2z_hash = H2Z_AK
         else:
-            h2z_hash = __CONV_TABLE['H2Z_K']
-            text = _conv_dakuten(text)
+            h2z_hash = H2Z_A
+    elif digit:
+        if kana:
+            h2z_hash = H2Z_DK
+        else:
+            h2z_hash = H2Z_D
+    else:
+        h2z_hash = H2Z_K
+    if kana:
+        text = _conv_dakuten(text)
     h2z_hash = _exclude_ignorechar(ignore, h2z_hash)
     return _convert(text, h2z_hash)
 
@@ -216,26 +158,23 @@ def z2h(text, ignore='', kana=True, ascii=False, digit=False):
     Return:
         <unicode> converted_text
     """
-    if kana and ascii and digit:
-        z2h_hash = __CONV_TABLE['Z2H_ALL']
-    else:
-        if ascii:
-            if digit:
-                if kana:
-                    z2h_hash = __CONV_TABLE['Z2H_ALL']
-                else:
-                    z2h_hash = __CONV_TABLE['Z2H_AD']
-            elif kana:
-                z2h_hash = __CONV_TABLE['Z2H_AK']
-            else:
-                z2h_hash = __CONV_TABLE['Z2H_A']
-        elif digit:
+    if ascii:
+        if digit:
             if kana:
-                z2h_hash = __CONV_TABLE['Z2H_DK']
+                z2h_hash = Z2H_ALL
             else:
-                z2h_hash = __CONV_TABLE['Z2H_D']
+                z2h_hash = Z2H_AD
+        elif kana:
+            z2h_hash = Z2H_AK
         else:
-            z2h_hash = __CONV_TABLE['Z2H_K']
+            z2h_hash = Z2H_A
+    elif digit:
+        if kana:
+            z2h_hash = Z2H_DK
+        else:
+            z2h_hash = Z2H_D
+    else:
+        z2h_hash = Z2H_K
     z2h_hash = _exclude_ignorechar(ignore, z2h_hash)
     return _convert(text, z2h_hash)
 
