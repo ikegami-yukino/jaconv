633eefe0cfb4aaaa63d252050044165b7c8e6e7f
山田一権 <k-yamada@xware.co.jp>
2017-03-24 15:35:48 +0900

    [fix again] っ(xtsu) alphabet convertion fix bug : occur Typeerror when convert a sentence that ends with 'っ' . (example : 'あっ' , 'ぐっ')

--- jaconv/jaconv.py
+++ jaconv/jaconv.py
@@ -215,7 +215,7 @@ def kana2alphabet(text):
         text = list(text)
         tsu_pos = text.index('っ')
         if len(text) <= tsu_pos + 1:
-            return str(text[:-1]) + 'xtsu'
+            return ''.join(text[:-1]) + 'xtsu'
         text[tsu_pos] = text[tsu_pos + 1]
         text = ''.join(text)
     return text
